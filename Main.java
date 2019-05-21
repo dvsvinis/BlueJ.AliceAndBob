6079d07ab02b59b63e1592701391382e90a1e3d2
Donna Svinis <donnasvinis@zipcoders-MacBook-Pro-3.local>
2019-05-21 17:04:05 -0400

    problem solved

--- Main.java
+++ Main.java
@@ -1,9 +1,18 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
+
 public class Main {
 
+   
     public static void main(String[] args ){
-
+        Scanner in = new Scanner(System.in);  // declare scanner
+        System.out.println("What is your name? "); //prompt user for name
+        String name = in.nextLine();  //reads input and assigns to name
+        if (name.equals("Alice") || name.equals("Bob")) {    //conditional
+            System.out.print("Greetings " + name);  //print message to screen 
+        }
+        
     }
 }
